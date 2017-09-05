bdbd0e042ef3cba3c102abc53f241ce3f2e651be
arukavytsia <artem.rukavitsya@gmail.com>
2017-09-05 17:16:05 +0300

    Spark structured streaming and Kafka integration

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,8 @@
 object Dependencies {
   object versions {
     val sparkVersion = "2.1.0"
+    val sparkLatestVersion = "2.2.0"
     val scalaKafkaClient = "0.11.0.0"
-    val akkaStreamsKafka = "0.17"
+    val akkaStreamsKafkaVersion = "0.17"
   }
 }
